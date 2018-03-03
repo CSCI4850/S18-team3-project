61ea9dba77b2f03143895351e6f4374a14e604d3
sremedios <sam.remedios@gmail.com>
2018-03-03 13:15:41 -0600

    calculates probabilities, returns them

--- utils/seq_likelihood/src/main.rs
+++ utils/seq_likelihood/src/main.rs
@@ -14,16 +14,35 @@
 use std::collections::HashMap;
 
 fn main() {
-    let transitions = vec!['A', 'B', 'B', 'C', 'B', 'A', 'D', 'D', 'A', 'B', 'A', 'D'];
+    let input_string = vec!['A', 'B', 'B', 'C', 'B', 'A', 'D', 'D', 'A', 'B', 'A', 'D']
+        .iter()
+        .map(|c| c.to_string())
+        .collect();
 
-    let mut hashmap = HashMap::new();
-    let num_elements = transitions.len();
+    let transitions = get_transition_likelihood(input_string);
+    println!("{:?}", transitions);
+}
+
+fn get_transition_likelihood(input_string: Vec<String>) -> HashMap<String, HashMap<String, f32>> {
+    let mut transitions = HashMap::new();
+    let mut totals = HashMap::new();
+    let num_elements = input_string.len();
     for i in 0..num_elements - 1 {
-        hashmap
-            .entry(transitions[i].clone())
-            .or_insert(Vec::new())
-            .push(transitions[i + 1].clone());
+        *transitions
+            .entry(input_string[i].clone())
+            .or_insert(HashMap::new())
+            .entry(input_string[i + 1].clone())
+            .or_insert(0_f32) += 1_f32;
+        *totals.entry(input_string[i].clone()).or_insert(0_f32) += 1_f32;
     }
 
-    println!("{:?}", hashmap);
+    // Convert next words into probabilities
+    for (cur_word, transitions) in transitions.iter_mut() {
+        for (_next_word, val) in transitions.iter_mut() {
+            *val /= totals[&*cur_word];
+        }
+    }
+
+    // return transition probabilities
+    transitions
 }
