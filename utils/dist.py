8a13d7d3f7038d4eeea56657157451787ef1e081
Michael Schmidt <Michael.Schmidt82@gmail.com>
2018-04-05 19:10:14 -0500

    minor rev, changed exception position and error msg

--- utils/dist.py
+++ utils/dist.py
@@ -2,14 +2,14 @@ from math import sqrt
 import numpy as np
 
 X = np.array([0, 1, 2, 3])
-Y = np.array([0, 1, 2, 4])
+Y = np.array([0, 1, 2, 5])
 
 def dist (X, Y):
+    if X.shape != Y.shape:
+        raise ValueError('Vectors must be of same dimensions')
+
     sum = 0
     Z = X - Y
-
-    if X.shape != Y.shape:
-        raise ValueError('Vectors must be contain the same dimensions')
 
     for i in range(X.shape[0]):
         sum += pow(Z[i], 2)
