d578bd5b7d257e50eb77405b8e4e827ec3c3d6fb
Michael Schmidt <Michael.Schmidt82@gmail.com>
2018-04-05 19:06:28 -0500

    used numpy functions which should be faster

--- utils/dist.py
+++ utils/dist.py
@@ -6,13 +6,15 @@ Y = np.array([0, 1, 2, 4])
 
 def dist (X, Y):
     sum = 0
+    Z = X - Y
 
     if X.shape != Y.shape:
         raise ValueError('Vectors must be contain the same dimensions')
 
     for i in range(X.shape[0]):
-        sum += pow(X[i] - Y[i], 2)
+        sum += pow(Z[i], 2)
 
     return sqrt(sum)
+
 
 print(dist(X, Y))
